daaf8e5fa366f19a937ba9a20a641e54a5c5b520
Pratyai Mazumder <pmazumder@ethz.ch>
2024-10-18 16:00:18 +0200

    Rerun things

--- tests/npbench/misc/cavity_flow_test.sh
+++ tests/npbench/misc/cavity_flow_test.sh
@@ -11,3 +11,19 @@ rm -rf tests/npbench/misc/_cpugraphs
 mv _dacegraphs tests/npbench/misc/_cpugraphs
 
 mv *DeviceType*.json tests/npbench/misc/
+
+
+
+SUFFIX="w_CMF_w_gsl"
+
+rm -rf "tests/npbench/misc/_cpugraphs_${SUFFIX}"
+mv tests/npbench/misc/_cpugraphs "tests/npbench/misc/_cpugraphs_${SUFFIX}"
+rm -rf "tests/npbench/misc/_gpugraphs_${SUFFIX}"
+mv tests/npbench/misc/_gpugraphs "tests/npbench/misc/_gpugraphs_${SUFFIX}"
+mv tests/npbench/misc/benchmark_cpu.md "tests/npbench/misc/benchmark_cpu_${SUFFIX}.text"
+mv tests/npbench/misc/benchmark_gpu.md "tests/npbench/misc/benchmark_gpu_${SUFFIX}.text"
+
+mv tests/npbench/misc/pre_DeviceType.CPU.json "tests/npbench/misc/pre_DeviceType.CPU_${SUFFIX}.json"
+mv tests/npbench/misc/pre_DeviceType.GPU.json "tests/npbench/misc/pre_DeviceType.GPU_${SUFFIX}.json"
+mv tests/npbench/misc/post_DeviceType.CPU.json "tests/npbench/misc/post_DeviceType.CPU_${SUFFIX}.json"
+mv tests/npbench/misc/post_DeviceType.GPU.json "tests/npbench/misc/post_DeviceType.GPU_${SUFFIX}.json"
